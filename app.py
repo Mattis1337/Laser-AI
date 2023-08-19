7c52a870aae0d90c2ae7541a48c0bdb638410dc1
Mattis1337 <130246549+Mattis1337@users.noreply.github.com>
2023-08-19 20:56:01 +0200

    Added closest feature
    I added the closest function which will determine which number based on a set of values (values) is closest to  a different given value (x). This function later needs to be added to the actual network or (read TODO).
    
    Hope this can be a sign of our starting of the project.
    
    Regards Mattis

--- app.py
+++ app.py
@@ -1,1 +1,29 @@
 print("Laser-AI")
+
+def closest(x, values):
+    """
+    This function will return the closest value/ number to a given value
+    :param x: (random) value that is to be inspected
+    :param values: list of values x is to be compared to
+    :return: value closest to x
+    """
+
+    #TODO: This function need to be added to the actual network. More in depth discussion about in the next meeting.
+
+    for i, value in enumerate(values):
+
+        if i == 0:
+            closest = value
+            p_diff = abs(value-x)   # p_diff is the closest difference measured yet
+            continue
+
+        diff = abs(value - x)   # diff is the difference of the current value to x
+        print(value)
+        print('-------')
+        print(diff)
+
+        if diff < p_diff:
+            closest = value
+            p_diff = diff
+
+    return closest
