45702d564d881f26135cec670b370e50a94a1911
Mattis1337 <130246549+Mattis1337@users.noreply.github.com>
2023-10-16 14:58:14 +0200

    Cleaning for the merge
    Cleaned up some code bits I used for testing which I forgot to remove with the last commit

--- app.py
+++ app.py
@@ -3,11 +3,8 @@ import numpy as np
 
 # importing own files
 from annotation_converter import *
-#import train_model
+import train_model
+
 
 print("Laser-AI")
 
-print("\n")
-bitboard = fen_to_bitboard("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
-
-print_bitboard_fen(bitboard)
