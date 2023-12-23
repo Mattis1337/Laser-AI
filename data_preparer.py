9324afa546d66d346129e5811ef7be9317077cce
Mattis1337 <130246549+Mattis1337@users.noreply.github.com>
2023-12-23 23:44:05 +0100

    Structuring data_preparer.py
    -Divided into black and white Datasets
    -Added Comments for next changes/ plans
    
    More in depth changes will happen once I got Arch running and wont have to keep using my phone.

--- data_preparer.py
+++ data_preparer.py
@@ -4,8 +4,11 @@ from torchvision.io import read_image
 
 # TODO: Find out what the custom Dataset is inheriting from and what output getitem fn has
 
-class CustomImageDataset(Dataset):
+# TODO: Convert all games from Dataset to bitboards in the CSV file to save resources
+#  do so once in annotation converter
+class WhiteMovesDataset(Dataset):
     def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
+        # Get the file containing all white moves
         self.img_labels = pd.read_csv(annotations_file)
         self.img_dir = img_dir
         self.transform = transform
@@ -24,3 +27,24 @@ class CustomImageDataset(Dataset):
             label = self.target_transform(label)
         return image, label
 
+
+class BlackMovesDataset(Dataset):
+    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
+        # Get a file containing all black moves 
+        self.img_labels = pd.read_csv(annotations_file)
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        image = read_image(img_path)
+        label = self.img_labels.iloc[idx, 1]
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return image, label
