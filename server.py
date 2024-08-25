9e8378d400022fd0551fc36804782fbbcf8803a6
Samuil Petrov <samuil.petrov@maxe-del.de>
2024-08-25 23:17:08 +0200

    Server: Make move generation async

--- server.py
+++ server.py
@@ -2,11 +2,15 @@
 #from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
+# Deps for async AI usage
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
 # AI interaction
 import chess
 import train_model
 
 
+executor = ThreadPoolExecutor()
 app = FastAPI()
 
 
@@ -20,7 +24,7 @@ class MoveResponse(BaseModel):
 
 @app.post("/predict", response_model=MoveResponse)
 async def get_prediction_request(request: PredictRequest):
-    move = predict_move(fen=request.fen, depth=request.depth)
+    move = await asyncio.get_event_loop().run_in_executor(executor, predict_move, request.fen)
     return MoveResponse(move)
 
 
@@ -28,12 +32,8 @@ def predict_move(fen: str, depth: int = 1):
     board = chess.Board(fen)
     ai_moves = train_model.generate_move(color=board.turn, fen=fen, amount_outputs=depth)
 
-    move = None
     for ai_move in ai_moves:
         if ai_move in board.legal_moves:
-            move = ai_move
-            break
-    if not move:
-        return predict_move(fen, depth + 1)
-         
-    return move
+            return ai_move
+
+    return predict_move(fen, depth + 1)
