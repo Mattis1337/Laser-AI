d0c5413729056fb75081faa4383fbee3ef90d576
Samuil Petrov <samuil.petrov@maxe-del.de>
2024-08-25 20:13:45 +0200

    Server: Add first implementation of REST API The server uses FastAPI to register sockets and manage datatypes. Our work is just to register the events like GET requests, calculate moves and then send them back. All using the Python format

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Laser-AI
 A chess AI that plays like a pro!
 
-## Dependencies
-* `numpy`
-* `python-chess`
-* `torch` `torchvision` `torchaudio`
-* `pandas`
-* `requests`
+## How to use
+1. **Initialize venv**: `python -m venv venv`
+2. **Source venv**: `source ./venv/bin/activate`
+3. **Install dependencies**: `pip install numpy python-chess torch pandas requests "fastapi[standard]"`
+    * _4.1_ **server**: `fastapi run server.py`
+    * _4.2_ **client**: ``
---
+++ server.py
@@ -0,0 +1,39 @@
+# REST API
+#from typing import Union
+from fastapi import FastAPI
+from pydantic import BaseModel
+# AI interaction
+import chess
+import train_model
+
+
+app = FastAPI()
+
+
+class PredictRequest(BaseModel):
+    fen: str
+    depth: int
+
+
+class MoveResponse(BaseModel):
+    move: str
+
+
+@app.get("/predict", response_model=MoveResponse)
+async def get_prediction_request(request: PredictRequest):
+    move = predict_move(fen=request.fen, depth=request.depth)
+    return MoveResponse(move)
+
+
+def predict_move(fen: str, depth: int):
+    board = chess.Board(fen)
+    ai_moves = train_model.generate_move(color=board.turn, fen=fen, amount_outputs=depth)
+
+    for ai_move in ai_moves:
+        if ai_move in board.legal_moves:
+            move = ai_move
+            break
+    if not move:
+        return predict_move(fen, depth + 1)
+         
+    return move
