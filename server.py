8f4daf7a5547b43ccd8d507726bd579828958558
Samuil Petrov <samuil.petrov@maxe-del.de>
2024-08-30 00:19:16 +0200

    Server: Add documentation

--- server.py
+++ server.py
@@ -14,6 +14,7 @@ executor = ThreadPoolExecutor()
 app = FastAPI()
 
 
+# parse JSON to objects of Python
 class PredictRequest(BaseModel):
     fen: str
 
@@ -24,11 +25,42 @@ class MoveResponse(BaseModel):
 
 @app.post("/predict", response_model=MoveResponse)
 async def get_prediction_request(request: PredictRequest):
+    """
+    Accept POST request to domain:port/predict and parse JSON to PredictRequest object
+
+    Args:
+        request (PredictRequest): Python object of class BaseModel containing JSON data 
+
+    Returns:
+        Dict[str, str]: The move in standardized JSON format defined in MoveResponse
+    """
+    # run AI on seperate coroutine preventing blockage of event loop
     move = await asyncio.get_event_loop().run_in_executor(executor, predict_move, request.fen)
-    return { "move": move }
+    return { "move": move } # standardized response 
 
 
 def predict_move(fen: str, depth: int = 1):
+    """
+    Predict move by running AI or selecting some legal move as fallback.
+    This function parses the FEN provided by the client. If the FEN is valid,
+    the AI model of the currently playing color is loaded and moves are requested.
+    They are sorted by the "likeliness" of them being the answer to the corresponding
+    FEN in the dataset. Because of this, the moves are looped through and the first
+    playable move is returned.
+
+    Args:
+        fen (str): FEN string to pass to the AI and validate moves from
+        depth (int, optional): Amount of outputs/moves the AI should return. Defaults to 1.
+
+    Raises:
+        HTTPException: When FEN is invalid
+        HTTPException: When there are no legal moves
+        HTTPException: When the AI is not available
+
+    Returns:
+        str: The move in Algebraic Notation
+    """
+    # Catch malformed FEN
     try:
         board = chess.Board(fen)
     except ValueError:
@@ -37,14 +69,14 @@ def predict_move(fen: str, depth: int = 1):
             detail="Invalid FEN code",
         )
 
+    # Catch impossible move generation
     if board.legal_moves.count() == 0:
         raise HTTPException(
             status_code=403,
             detail="No moves possible"
         )
 
-    # TODO(Samuil): Isn't a loop that generates
-    # all previous moves again + 1 more move very inefficient
+    # Catch AI errors
     try:
         ai_moves = train_model.generate_move(color=board.turn, fen=fen, amount_outputs=depth)
     except:
@@ -53,6 +85,7 @@ def predict_move(fen: str, depth: int = 1):
             detail="AI not available"
         )
 
+    # Get first legal move of outputs
     for ai_move in ai_moves:
         try:
             move = board.parse_san(ai_move)
@@ -62,4 +95,5 @@ def predict_move(fen: str, depth: int = 1):
             return ai_move
 
     # Make legal_move generator a list, get the first move and then convert it to SAN
+    # In other words: get legal move, even if bad
     return board.san(list(board.legal_moves)[0])
