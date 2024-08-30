9bf13d10cff0dc873d4eb8f21dfdc4bcf681d6e9
Samuil Petrov <samuil.petrov@maxe-del.de>
2024-08-31 01:26:42 +0200

    Scraper: Add type hints

--- scraper.py
+++ scraper.py
@@ -42,7 +42,7 @@ json_header = {
 }
 
 
-def get_games_pgn(username: str):
+def get_games_pgn(username: str) -> list[str]:
     """
     Downloads all public games that a Chess.com account has ever played as PGN
     :param username: The user to download games from
@@ -51,12 +51,12 @@ def get_games_pgn(username: str):
 
     # builds the player's game archive URL which is an index of all games
     url = f"https://api.chess.com/pub/player/{username}/games/archives"
-    filtered_games = []
+    filtered_games: list[str] = []
     
     try:
         request = requests.get(url=url, headers=json_header)
         request.raise_for_status()
-        archives = request.json()["archives"]
+        archives: list[str] = request.json()["archives"]
 
     except (requests.RequestException, KeyError) as error:
         print(f"Failed to GET archives of {username} with error: {error}")
@@ -68,7 +68,7 @@ def get_games_pgn(username: str):
         try:
             request = requests.get(url=archive, headers=json_header)
             request.raise_for_status()
-            games = request.json()["games"]
+            games: list[dict[str, str]] = request.json()["games"]
 
         except (requests.RequestException, KeyError) as error:
             print(f"Failed to GET games from '{archive}' with error: {error}")
@@ -89,11 +89,11 @@ def get_games_pgn(username: str):
     return filtered_games
 
 
-def save_game(game_pgn):
+def save_game(game_pgn: str) -> None:
 
     try:
         timestamp = int(time.time())
-        file_path = os.path.join(save_dir, f"{timestamp}.pgn")
+        file_path: str = os.path.join(save_dir, f"{timestamp}.pgn")
         with open(file_path, "w") as file:
             file.write(game_pgn)    # /home/user/Downloads/Games/UnixTime.pgn
 
@@ -101,8 +101,8 @@ def save_game(game_pgn):
         print("An IO error occurred while saving a game: " + error)
 
 
-def process_games(player: str):
-    games = get_games_pgn(player)
+def process_games(player: str) -> None:
+    games: list[str] = get_games_pgn(player)
     if not games or games.count() == 0:
         print(f"No games acquired for player {player}")
         return
