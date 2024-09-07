b08e777ed85694c7ea294e5725c4ba58e079a53e
mattis <mattis.backhaus@maxe-del.de>
2024-09-07 15:35:55 +0200

    train_model: changed batch size from 64 to 128 in training dataloader
    Signed-off-by: mattis <mattis.backhaus@maxe-del.de>

--- train_model.py
+++ train_model.py
@@ -134,7 +134,7 @@ def train_chess_model(dataset: datasets.ChessDataset, epochs: int) -> None:
     # Reference for handling changing dimensions
     # https://discuss.pytorch.org/t/how-to-load-a-dpretrained-model-with-a-different-output-dimension/26117/7
 
-    train_dataloader = DataLoader(dataset, batch_size=64, shuffle=True, num_workers=8)
+    train_dataloader = DataLoader(dataset, batch_size=128, shuffle=True, num_workers=8)
     test_dataloader = DataLoader(dataset, shuffle=True, num_workers=8)
 
     # loading the last checkpoints
