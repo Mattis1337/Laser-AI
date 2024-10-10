c877459642f27a06bd6fc7677ba0f84bc77bbea5
mattis <mattis.backhaus@maxe-del.de>
2024-10-10 15:34:15 +0200

    [BUG FIX] models: added ReLU activation function to all seqentials after the first convolutional layer as it was missing before and removed it after the MaxPool2D layer; added new topology to test
    Signed-off-by: mattis <mattis.backhaus@maxe-del.de>

--- models.py
+++ models.py
@@ -21,15 +21,15 @@ class RetroLaserAI(nn.Module):
         The following script calculates the number of input channels for fc1.
 
         # calculate the input channel for fully connected layer 1
-        x, y = dataset.__getitem__(0)
-        conv1 = nn.Conv2d(12, 24, 5)
-        pool = nn.MaxPool2d(2, 2)
-        conv2 = nn.Conv2d(24, 32, 2)
+        from torch import nn
+        x = torch.randn([0, 12, 8, 8])
+        conv1 = nn.Conv2d(12, 24, 6)
+        conv2 = nn.Conv2d(24, 48, 3)
+        print(x.shape)
         x = conv1(x)
-        x = pool(x)
+        print(x.shape)
         x = conv2(x)
         print(x.shape)
-        # based of the dimension of 32x1x1 one can deduce that only 1 pooling layer is needed
         """
         super().__init__()
         # 12 input channels for 12 different piece types
@@ -70,19 +70,30 @@ def init_neural_network(outputs: int):
     Initialising a Neural Network using a specified topology / Sequential.
     :param outputs: number of outputs for the last layer
     """
-    conv_seq = STANDARD_TOPOLOGY[0]
-    fc_seq = STANDARD_TOPOLOGY[1]
+    conv_seq = PADDED_CONV_TOPOLOGY[0]
+    fc_seq = PADDED_CONV_TOPOLOGY[1]
 
     model = NeuralNetwork(conv_seq, fc_seq, outputs)
 
     return model
 
 
+def get_output_shape(model, image_dim):
+    """
+    Returns torch.Size object to the last layer of a given model by providing it with dummy data and passing
+    it through. The model will change the data and its dimension based on its different layers.
+    :param model: the model topology
+    :param image_dim: the dimensions of the dummy data
+    """
+    x = model(torch.rand(image_dim))
+    return np.shape(x)
+
+
 STANDARD_TOPOLOGY: list[nn.Sequential()] = [
     nn.Sequential(
         nn.Conv2d(12, 24, 5),
-        nn.MaxPool2d(2, 2),
         nn.ReLU(),
+        nn.MaxPool2d(2, 2),
         nn.Conv2d(24, 32, 2),
         nn.ReLU(),
     ),
@@ -99,8 +110,8 @@ STANDARD_TOPOLOGY: list[nn.Sequential()] = [
 PADDED_CONV_TOPOLOGY: list[nn.Sequential()] = [
     nn.Sequential(
         nn.Conv2d(12, 24, 5, padding=1),
-        nn.MaxPool2d(2, 2),
         nn.ReLU(),
+        nn.MaxPool2d(2, 2),
         nn.Conv2d(24, 48, 3),
         nn.ReLU(),
     ),
@@ -112,13 +123,15 @@ PADDED_CONV_TOPOLOGY: list[nn.Sequential()] = [
     )
 ]
 
-
-def get_output_shape(model, image_dim):
-    """
-    Returns torch.Size object to the last layer of a given model by providing it with dummy data and passing
-    it through. The model will change the data and its dimension based on its different layers.
-    :param model: the model topology
-    :param image_dim: the dimensions of the dummy data
-    """
-    x = model(torch.rand(image_dim))
-    return np.shape(x)
+# Removing the Pooling layer might lead to better results as pooling layers
+# are generally being used to blurr out noise however on a chess board almost every feature
+# plays a vital role to the move that should be generated
+PADDED_NOPOOL_TOPOLOGY: list[nn.Sequential()] = [
+    nn.Sequential(
+        nn.Conv2d(12, 24, 6),
+        nn.ReLU(),
+        nn.Conv2d(24, 48, 3),
+        nn.ReLU(),
+    ),
+    PADDED_CONV_TOPOLOGY[1]
+]
