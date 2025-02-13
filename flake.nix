f44d2a9a32739de90aebdb1955f11d824a59eef6
Samuil Petrov <samuil.petrov@maxe-del.de>
2025-02-13 19:57:55 +0100

    Nix: Convert shell.nix to flake.nix

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Flake for the Laser-AI development shell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }: let
+    pkgs = nixpkgs.legacyPackages."x86_64-linux";
+  in {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+
+      packages = [
+        (pkgs.python3.withPackages(p: with p; [
+          numpy
+          chess
+          torch
+          pandas
+          requests
+          fastapi
+        ]))
+      ];
+
+    };
+  };
+}
+
