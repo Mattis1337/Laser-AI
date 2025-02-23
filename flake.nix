8fd4aa4b6b53c44d63da92c32c83543f0b92fb94
Samuil Petrov <samuil.petrov@maxe-del.de>
2025-02-23 16:09:18 +0100

    Nix: Add support for CUDA

--- flake.nix
+++ flake.nix
@@ -5,20 +5,29 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   outputs = { self, nixpkgs, ... }: let
+    system = "x86_64-linux";
     pkgs = import nixpkgs {
-      system = "x86_64-linux";
-      #config.allowUnfree = true;
-      #config.cudaSupport = true;
+      system = system;
+      config.allowUnfree = true;
     };
   in {
-    devShells.x86_64-linux.default = pkgs.mkShell {
+    devShells.${system}.default = pkgs.mkShell {
 
       packages = [
         (pkgs.python313.withPackages(p: with p; [
           numpy
           chess
-          torch
+          torchWithCuda
           pandas
           requests
           fastapi
@@ -29,4 +38,3 @@
     };
   };
 }
-
